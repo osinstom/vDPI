0b2458b53c49bc3d78f59a46473fc066f971066e
Tomek Osiński <osinstom@gmail.com>
2018-09-26 12:14:01 +0200

    Updated init script

--- init_script.sh
+++ init_script.sh
@@ -2,11 +2,12 @@
 
 sudo apt-get update
 
-sudo apt-get install python-pip libpcap-dev libnetfilter-queue-dev git openvswitch-switch
+sudo apt-get -qq -y install python-pip git openvswitch-switch
 
 IFACE=$(ifconfig -a | awk '/ens/ {print $1}' | sed -n 2p)
 
 echo -e "auto ${IFACE}\niface ${IFACE} inet dhcp" | sudo tee /etc/network/interfaces.d/${IFACE}.cfg > /dev/null
+sudo ifup ${IFACE}
 
 git clone http://10.254.188.33/diaas/vDPI.git
 cd vDPI
@@ -17,7 +18,7 @@ sudo ovs-vsctl add-br br0
 sudo ovs-vsctl add-port br0 ${IFACE}
 sudo ovs-ofctl add-flow br0 in_port=1,action=in_port
 
-sudo python vdpi.py
+sudo python vdpi.py ${IFACE}
 
 
 
