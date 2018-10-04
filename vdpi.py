994c75047d81dc207654446e7193ef00545e5eba
Tomek Osiński <osinstom@gmail.com>
2018-10-04 14:22:01 +0200

    add

--- vdpi.py
+++ vdpi.py
@@ -3,8 +3,7 @@
 from scapy.all import *
 from scapy.layers.inet import TCP, IP
 from scapy_http import http
-from scapy_ssl_tls.ssl_tls import TLS, SSL
-from scapy.layers.ssl_tls import TLSRecord, SSLv2Record
+from scapy_ssl_tls.ssl_tls import TLS, SSL, TLSRecord, SSLv2Record
 import sys
 import logging
 
