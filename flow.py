4d0431088492ca36eac6ca873566b9aafaef6535
Tomek Osiński <osinstom@gmail.com>
2018-10-05 16:50:42 +0200

    Update

--- flow.py
+++ flow.py
@@ -10,6 +10,12 @@ class Flow(object):
     def __str__(self):
         return "%s %s %s" % (self.src_ip, self.dst_ip, self.proto)
 
+    def __eq__(self, other):
+        """Overrides the default implementation"""
+        if isinstance(other, Flow):
+            return self.src_ip == other.src_ip and self.dst_ip == other.dst_ip and self.proto == other.proto
+        return False
+
 
 class L4Flow(Flow):
 
