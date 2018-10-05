9153027656b626c32fb5b2d4fd419a7daba648c1
Tomek Osiński <osinstom@gmail.com>
2018-10-05 16:22:28 +0200

    Update

--- flow.py
+++ flow.py
@@ -2,7 +2,12 @@
 class Flow(object):
 
     def __init__(self, **kwargs):
-        self.src_ip = kwargs['src_ip']
-        self.dst_ip = kwargs['dst_ip']
-        self.src_port = kwargs['src_port']
-        self.dst_port = kwargs['dst_port']
+        if kwargs:
+            self.src_ip = kwargs['src_ip']
+            self.dst_ip = kwargs['dst_ip']
+            self.src_port = kwargs['src_port']
+            self.dst_port = kwargs['dst_port']
+            self.proto = kwargs['proto']
+
+    def __str__(self):
+        return "%s %s %s %s %s" % (self.src_ip, self.dst_ip, self.src_port, self.dst_port, self.proto)
--- vdpi.py
+++ vdpi.py
@@ -1,18 +1,37 @@
-#! /usr/bin/env python2.7
-
 from scapy.all import *
 from scapy.layers.inet import TCP, IP
 from scapy_http import http
 from scapy_ssl_tls.ssl_tls import TLS, SSL, TLSRecord, SSLv2Record
+from scapy.layers.ssl_tls import TLS, SSL, TLSRecord, SSLv2Record
 import sys
 import logging
 
+from flow import Flow
+
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.DEBUG)
+
+flows = []
+
+
+def is_new_flow(pkt):
+    return True
+
+
+def add_flow(pkt):
+    flow = Flow()
+    flows.append(flow)
+
+
+def debug_packet(pkt):
+    print "Packet received: %s, %s -> %s" % (pkt[IP].proto, pkt[IP].src, pkt[IP].dst)
 
 
 def callback(pkt):
-    pkt.show() # debug statement
+    debug_packet(pkt)
     if pkt.haslayer(TCP) and pkt.haslayer(Raw):
+        if is_new_flow(pkt):
+            add_flow(pkt)
+
         if pkt.haslayer(TLSRecord) or pkt.haslayer(SSLv2Record):
             print "ENCRYPTED PACKET!"
         elif pkt.haslayer('HTTP'):
@@ -21,8 +40,15 @@ def callback(pkt):
             print "Some other packet.."
 
 
+def print_summary():
+    print "SUMMARY"
+
+
 def main(intf):
-    sniff(iface=intf, prn=callback, filter="tcp", store=0)
+    try:
+        sniff(iface=intf, prn=callback, filter="", store=0)
+    except KeyboardInterrupt:
+        print_summary()
 
 
 if __name__ == "__main__":
